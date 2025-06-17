cbf4fb302deb3c8b9667d355e419c30d23dd161c
akbar farooq <akbarfarooq2006@gmail.com>
2025-06-17 11:16:48 +0500

    Mood tracker

---
+++ mood-traceker/main.py
@@ -0,0 +1,64 @@
+import streamlit as st
+import pandas as pd
+import datetime
+import csv
+import os
+
+# st.title("Mood Tracker 😎😎")
+
+MOOD_FILE = "mood_log.csv"
+
+def load_mood_data():
+    if not os.path.exists(MOOD_FILE):
+        return pd.DataFrame(columns=["Date", "Mood"])
+    return pd.read_csv(MOOD_FILE, encoding = "utf-8")
+
+
+def save_mood_data(date,mood):
+    with open(MOOD_FILE, "a" , encoding = "utf-8" ) as file:
+        writer = csv.writer(file)
+        writer.writerow([date, mood])
+        
+
+st.title("Mood Tracker 😎😎")
+st.subheader ("How are you felling today?")
+
+today = datetime.date.today()
+mood = st.selectbox("Select your mood:", ["Happy", "Sad", "Angry", "Neutral"])
+
+if st.button("Log Mood"):
+    if mood == "Happy":
+        save_mood_data(datetime.date.today(), mood+"😀")
+    elif mood == "Sad":
+        save_mood_data(datetime.date.today(), mood+"😔")
+    elif mood == "Angry":
+        save_mood_data(datetime.date.today(), mood+"😠")
+    else:
+        save_mood_data(datetime.date.today(), mood+"😐")
+    st.success("Mood saved successfully!")
+
+
+data = load_mood_data()
+# st.write(data)
+if not data.empty:
+    st.subheader("Mood Trends over time")
+    # data["Date"] = pd.to_datetime(data["Date"])
+    
+    mood_counts = data.groupby("Mood").count()["Date"]
+    st.bar_chart(mood_counts)
+    st.write(mood_counts)
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
